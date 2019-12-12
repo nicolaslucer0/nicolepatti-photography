d6a30e14dc21e7f15f7ab5a2e994080e1178c3c6
nlucero <nlucero@glamit.com.ar>
2019-12-11 23:18:11 -0300

    prevent right click on modal and images

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    $('.flex-image').on('click', function(){
+        _this = $(this);
+        $('.modal').show();
+        $('.modal').find("img").attr("src", _this.attr("src"));
+    });
+
+      $('img').mousedown(function (e) {
+        if(e.button == 2) { // right click
+          return false; // do nothing!
+        }
+      });
+
+      $("img").on("contextmenu",function(){
+        return false;
+     });
+
+      $(".modal").on("contextmenu",function(){
+        return false;
+     });
+});
